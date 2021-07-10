1b234fa15522e59515a5817bde54bbb5e5a88d71
Scienthusiast <timotheebehra.etu@gmail.com>
2021-07-10 14:52:50 +0200

    Background color in constants

--- src/init.js
+++ src/init.js
@@ -1,8 +1,6 @@
 
 const app = new PIXI.Application({
-    width: window.innerWidth,
-    height: window.innerHeight,
-    backgroundColor: BACKGROUND_COLOR,
+width: window.innerWidth, height: window.innerHeight, backgroundColor: BACKGROUND_COLOR
 });
 
 const container = new PIXI.Container();
