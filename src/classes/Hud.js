e65e38b677d32b886eeabb779eb5b8c8690aed91
Lola <lolivet@jolicode.com>
2021-07-11 02:31:51 +0200

    Settings in hud

--- src/classes/Hud.js
+++ src/classes/Hud.js
@@ -1,14 +1,9 @@
 class Hud {
-    constructor(player) {
+    constructor() {
         this.hud = new PIXI.Container();
         this.floor = FLOORS.START;
-        this.life = player.health;
-
-        this.drawSetting();
-    }
-
-    draw(player, enemies) {
-        let floorText = new PIXI.Text(this.floor, {
+        this.containerText = new PIXI.Graphics();
+        this.floorText = new PIXI.Text(this.floor, {
             fontFamily: 'Comic Sans MS',
             fontSize: 20,
             fill: 0x990201,
@@ -16,29 +11,65 @@ class Hud {
             wordWrap: true,
             wordWrapWidth: 230,
         })
-        console.log('XXXXX',window.game.map.minimap.x)
-        window.game.map.minimap.x += 1600;
-        window.game.map.minimap.y += 200;
+        this.commandsText = new PIXI.Text('W: moving forward\nS: moving backward\nA: moving left\nD: moving rigth\nMouse: shoot\nV: increment power\nC: decrement power', {
+            fontFamily: 'Comic Sans MS',
+            fontSize: 20,
+            fill: 0xFFFFFF,
+            fontWeight: 400,
+            wordWrap: true,
+            wordWrapWidth: 230,
+        })
+        this.isSettingsOpen = false;
+    }
+
+    draw(player, enemies) {
+        window.game.map.minimap.x += window.innerWidth;
+        window.game.map.minimap.y += window.innerHeigth;
         hud.addChild(window.game.map.minimap);
 
         hud.addChild(this.hud);
-        this.drawLifeBar(player.health)
-        console.log(enemies.length)
-        this.drawNbEnemiesLeft(enemies.length)
+
+        this.drawLifeBar(player.health);
+        this.drawNbEnemiesLeft(enemies.length);
+        this.drawSetting();
         
-        floorText.position.set(10, 5);
-        hud.addChild(this.settings);
-        hud.addChild(floorText);
+        this.floorText.position.set(10, 5);
+        hud.addChild(this.floorText);
     }
 
     drawSetting() {
-        this.settings = new PIXI.Sprite.from(textures.settings)
+        this.settings = new PIXI.Sprite.from(textures.settings);
         this.settings.anchor.set(0.5, 0.5);
         this.settings.x = window.innerWidth - 40;
         this.settings.y = window.innerHeight - 40;
         this.settings.width = 40;
         this.settings.height = 40;
         this.settings.interactive  = true;
+        this.settings.emit('pointerdown')
+        this.settings.on('pointerdown', this.handleSettings.bind(this));
+
+        this.containerText.beginFill(0x000000);
+        this.containerText.drawRect(window.innerWidth - 260, window.innerHeight -360, 200, 300)
+        this.containerText.endFill();
+        this.containerText.visible = this.isSettingsOpen
+
+        this.commandsText.position.set(window.innerWidth - 255, window.innerHeight -360);
+        this.commandsText.visible = this.isSettingsOpen;
+
+        hud.addChild(this.containerText);
+        hud.addChild(this.commandsText);
+        hud.addChild(this.settings);
+    }
+
+    handleSettings() {
+        if (this.isSettingsOpen) {
+            this.containerText.visible = false;
+            this.commandsText.visible = false;
+        } else {
+            this.containerText.visible = true;
+            this.commandsText.visible = true;
+        }
+        this.isSettingsOpen = !this.isSettingsOpen;
     }
 
     drawLifeBar(health) {
