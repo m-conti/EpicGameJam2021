56f32450835ab50dc0cc118cedc66b77a3850bad
Mattia Torsello <torsello.mattia@gmail.com>
2021-07-11 15:45:13 +0200

    maintenant on voit quand le boss nous fait des degats

--- src/classes/enemies/EnemyTrombi.js
+++ src/classes/enemies/EnemyTrombi.js
@@ -13,6 +13,7 @@ class ProjectileTrombi extends EnemyProjectile {
 
   onCollide(elementCollided) {
     elementCollided.health -= this.damage;
+    window.game.hud.drawLifeBar(this.health);
     this.remove();
     if (window.game.player.health < 0)
       window.game.gameOverGood()
